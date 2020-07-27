5fe2a02fe5ee02a75319c47be8d1d567ba6ff97f
Garrett Jones <garrettojones@gmail.com>
2020-07-27 16:24:40 -0500

    got react, database, and server all connected and running

---
+++ client/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (<h1>Hello World, React!</h1>);
+  }
+}
+
+// const App = () => <div>HELLO WORLDDDDDD FROM REACT!</div>;
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const { PORT } = process.env;
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mvp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+  .then(() => {
+    console.log("Connected to the DB");
+  })
+  .catch((err) => {
+    console.error('Error connecting to the DB');
+  });
+
+app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));

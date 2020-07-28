74036672b0d3f0f9b8562485a79bc3610b9c8253
Garrett Jones <garrettojones@gmail.com>
2020-07-28 02:54:12 -0500

    hitting correct post route but not getting right data in res

--- client/app.js
+++ client/app.js
@@ -22,10 +22,12 @@ class App extends Component {
     componentDidMount() {
         axios.get('https://pokeapi.co/api/v2/pokemon')
         .then(res => {
-            // console.log('RES', res);
+            // console.log('RES', res)
             this.setState({
                 allPokemon: res.data
             })
+            //get from database in another .then
+            //merge and update
         }).catch(err => console.log(err));
     }
 
@@ -37,17 +39,29 @@ class App extends Component {
             caughtPokemon.push(pokemon);
             allPokemon.results.splice(index, 1)
             this.setState({allPokemon: allPokemon});
+                // const params = {id: pokemon.id, name: pokemon.name, url: pokemon.url}
+            axios.post('http://localhost:8080/server/server', pokemon)
+                .then((res, req) => {
+                    console.log('RES.DATA', res)
+                    console.log(req)
+                }).catch((error) => {
+                    console.log('error', error)
+                });
         } else {
         null
         }
     }
+        // axios.post('/create', caughtPokemon)
+        // .then(res => console.log(res.data))
+        // .catch(err => console.log('ERR in axios post', err));
+    
 
     releaseEm(pokemon, index) {
         const { currentPokemon, caughtPokemon, allPokemon } = this.state;
         const filtered = caughtPokemon.filter(char => char !== pokemon);
         allPokemon.results.splice(index, 0, pokemon);
         this.setState({caughtPokemon: filtered, allPokemon: allPokemon});
-        console.log('released Pokemon chosen')
+        console.log('released Pokemon chosen') 
     }
 
   render() {
--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { PORT } = process.env;
+
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const pokemon = mongoose.model('mvp', pokeDex);
+const router = express.Router();
+const bodyParser= require('body-parser');
+app.use('/server/server', cors(), router)
 
 mongoose.connect('mongodb://localhost/mvp', {
     useNewUrlParser: true,
@@ -10,11 +17,65 @@ mongoose.connect('mongodb://localhost/mvp', {
 })
   .then(() => {
     console.log("Connected to the DB");
+    // const db = client.db('mvp');
   })
   .catch((err) => {
     console.error('Error connecting to the DB');
   });
 
+  let pokeDex = new Schema({
+    id: {
+      type: Number
+    },
+    name: {
+        type: String
+    },
+    url: {
+        type: String
+    }
+}, {
+        collection: 'users'
+    })
+
+const pokemon = mongoose.model('mvp', pokeDex);
+
+
+// const corsOptions = {
+//   origin: 'http://localhost',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+app.use(cors);
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => res.sendFile(`${__dirname}/index.html`));
+
+// create
+router.post('/', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
+  pokemon.create(req.body, (err, data) => {
+            if (err) {
+                console.log('err', err)
+            } else {
+                console.log(data)
+                res.json(data)
+            }
+        })
+})
+
+// router.route('/create').post((req, res, next) => {
+//     user.create(req.body, (error, data) => {
+//         if (error) {
+//             return next(error)
+//         } else {
+//             console.log(data)
+//             res.json(data)
+//         }
+//     })
+// });
+
+
+//delete
+
+
+//update
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
